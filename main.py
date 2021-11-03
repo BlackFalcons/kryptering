bf2b536f3205e9c61c6edc87ff8ee4acec13beb0
Eivind Limstrand <eivindlimstrand@gmail.com>
2021-11-03 20:47:38 +0100

    Replace encryption algorithm with bruteforce

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
+from Cracker import Cracker
 from Encryption import Encryption
 
 if __name__ == '__main__':
-    while True:
-        prompt_encryption = input("Vil du kryptere eller dekryptere(krypter eller dekrypter): ")
-        prompt_lowered = prompt_encryption.lower()
+    cracker = Cracker("Imzmrh", Encryption.getNorwegianAlphabet())
+    cracker.bruteforce()
 
-        if prompt_lowered == "krypter":
-            try:
-                key = int(input("Oppgi nøkkel: "))
-            except ValueError:
-                print("You have to provide a integer.")
-                break
-
-            msg = input("Skriv inn meldingen: ")
-
-            if isinstance(key, int):
-                print(Encryption.encrypt(msg, key))
-            else:
-                print("Du må oppgi et heltall!")
-            break
-        elif prompt_lowered == "dekrypter":
-            try:
-                key = int(input("Oppgi nøkkel: "))
-            except ValueError:
-                print("You have to provide a integer.")
-                break
-
-            msg = input("Skriv inn meldingen: ")
-
-            if isinstance(key, int):
-                print(Encryption.decrypt(msg, key))
-            else:
-                print("Du må oppgi et heltall!")
-            break
+    # while True:
+    #     prompt_encryption = input("Vil du kryptere eller dekryptere(k eller d): ")
+    #     prompt_lowered = prompt_encryption.lower()
+    #
+    #     if prompt_lowered == "k":
+    #         try:
+    #             key = int(input("Oppgi nøkkel: "))
+    #         except ValueError:
+    #             print("You have to provide a integer.")
+    #             break
+    #
+    #         msg = input("Skriv inn meldingen: ")
+    #
+    #         if isinstance(key, int):
+    #             print(Encryption.encrypt(msg, key))
+    #         else:
+    #             print("Du må oppgi et heltall!")
+    #         break
+    #     elif prompt_lowered == "d":
+    #         try:
+    #             key = int(input("Oppgi nøkkel: "))
+    #         except ValueError:
+    #             print("You have to provide a integer.")
+    #             break
+    #
+    #         msg = input("Skriv inn meldingen: ")
+    #
+    #         if isinstance(key, int):
+    #             print(Encryption.decrypt(msg, key))
+    #         else:
+    #             print("Du må oppgi et heltall!")
+    #         break
